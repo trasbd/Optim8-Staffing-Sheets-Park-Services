c5fba9d892f6419ce6bc19f2edc18ac050a6001e
Thomas Robert <thomas.m.robert@gmail.com>
2024-04-07 17:47:08 -0500

    recommit

--- certs/certs/obj/Debug/netcoreapp3.1/certs.AssemblyInfo.cs
+++ certs/certs/obj/Debug/netcoreapp3.1/certs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("certs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e37e6d847a2d3622103b8e672d228b23c5a3c5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("certs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("certs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
